7c632237b97a7c21c0829548c8da03afd11c882e
MuhammadHasan-04 <ha7971768@gmail.com>
2026-03-19 16:43:21 +0500

    Task4

---
+++ Task4/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This application demonstrates routing using React Router.</p>
+    </div>
+  );
+}
+
+export default About;
